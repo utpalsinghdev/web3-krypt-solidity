7ef33f941356df5c4d06b6bac5f937e52e8a0855
Utpal Singh <utpalsingh915@gmail.com>
2023-10-09 22:36:51 +0530

    transfer and count done

---
+++ contracts/Transactions.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionsCount;
+
+    event Transfer(
+        address from,
+        address reciever,
+        uint amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+    TransferStruct[] transactions;
+
+    function addToBlockChain(
+        address payable receiver,
+        uint amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionsCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionsCount() public view returns (uint256) {
+        return transactionsCount;
+    }
+}
